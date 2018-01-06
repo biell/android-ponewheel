ed67986472348ed649f0e2f64d4c2552a2d3faeb
ebabel <erik.babel@gmail.com>
2018-01-05 19:31:24 -0800

    WIP

--- app/src/main/java/net/kwatts/powtools/view/RideModeView.java
+++ app/src/main/java/net/kwatts/powtools/view/RideModeView.java
@@ -1,32 +1,21 @@
 package net.kwatts.powtools.view;
 
-import android.app.Activity;
 import android.databinding.Observable;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.support.v7.widget.CardView;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-
-import com.rey.material.widget.Button;
 
 import net.kwatts.powtools.MainActivity;
 import net.kwatts.powtools.R;
 import net.kwatts.powtools.model.OWDevice;
-import net.kwatts.powtools.util.BluetoothUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
 
-import timber.log.Timber;
 
 public class RideModeView {
 
     CardView rideModeCard;
-    final Activity activity;
+    final MainActivity activity;
     final OWDevice owDevice;
 
     public RideModeView(MainActivity activity, OWDevice owDevice) {
@@ -37,9 +26,6 @@ public class RideModeView {
 
         CardView.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        LinearLayout radioGroup = new LinearLayout(activity);
-        radioGroup.setOrientation(LinearLayout.HORIZONTAL);
-
         String[] rideModes;
         if (owDevice.isOneWheelPlus.get()) {
             rideModes = activity.getResources().getStringArray(R.array.owplus_ridemode_array);
@@ -48,72 +34,51 @@ public class RideModeView {
         }
         int rideModeButtonMargin = activity.getResources().getDimensionPixelSize(R.dimen.ride_mode_button_margin);
 
-        int position = 0;
-        final List<Button> buttons = new ArrayList<>();
-        for (String rideMode : rideModes) {
-            Button button = new Button(activity);
-            buttons.add(button);
+        MultiStateToggleButton multiStateToggleButton = new MultiStateToggleButton(activity);
+        multiStateToggleButton.setElements(rideModes);
 
-//            TextViewCompat.setAutoSizeTextTypeWithDefaults(button, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-            button.setGravity(Gravity.CENTER);
-            button.setMaxLines(1);
-
-            button.setText(rideMode);
-            LinearLayout.MarginLayoutParams textViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
-
-//            textViewParams.setMargins(0, rideModeButtonMargin, rideModeButtonMargin, rideModeButtonMargin);
-            radioGroup.addView(button, textViewParams);
-
-            final int positionIdentifier = position;
-            button.setOnClickListener(v -> onButtonPressed(owDevice, activity.getBluetoothUtil(), positionIdentifier));
-
-            position++;
-        }
+//        int position = 0;
+//        final List<Button> buttons = new ArrayList<>();
+//        for (String rideMode : rideModes) {
+//            Button button = new Button(activity);
+//            buttons.add(button);
+//
+//            button.setGravity(Gravity.CENTER);
+//            button.setMaxLines(1);
+//
+//            button.setText(rideMode);
+//            LinearLayout.MarginLayoutParams textViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+//
+//            radioGroup.addView(button, textViewParams);
+//
+//            final int positionIdentifier = position;
+//            button.setOnClickListener(v -> onButtonPressed(owDevice, activity.getBluetoothUtil(), positionIdentifier));
+//
+//            position++;
+//        }
         rideModeCard.setPadding(rideModeButtonMargin, rideModeButtonMargin,rideModeButtonMargin,rideModeButtonMargin);
-        rideModeCard.addView(radioGroup, params);
+        rideModeCard.addView(multiStateToggleButton, params);
 
         owDevice.isConnected.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
             @Override
             public void onPropertyChanged(Observable observable, int i) {
-                updateEnabledState(buttons);
+                updateEnabledState(multiStateToggleButton);
             }
         });
-        updateEnabledState(buttons);
+        updateEnabledState(multiStateToggleButton);
 
-
-//
-//
-//        mRideModeToggleButton = this.findViewById(R.id.mstb_multi_ridemodes);
-//        if (owDevice.isOneWheelPlus.get()) {
-//            mRideModeToggleButton.setElements(getResources().getStringArray(R.array.owplus_ridemode_array));
-//        } else {
-//            mRideModeToggleButton.setElements(getResources().getStringArray(R.array.ow_ridemode_array));
-//        }
-//
-//        mRideModeToggleButton.setOnValueChangedListener(position -> {
-//            if (owDevice.isConnected.get()) {
-//                Log.d(TAG, "owDevice.setRideMode button pressed:" + position);
-//                if (owDevice.isOneWheelPlus.get()) {
-//                    updateLog("Ridemode changed to:" + position + 4);
-//                    owDevice.setRideMode(getBluetoothUtil(),position + 4); // ow+ ble value for ridemode 4,5,6,7,8 (delirium)
-//                } else {
-//                    updateLog("Ridemode changed to:" + position + 1);
-//                    owDevice.setRideMode(getBluetoothUtil(),position + 1); // ow uses 1,2,3 (expert)
-//                }
-//            } else {
-//                Toast.makeText(mContext, "Not connected to Device!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        multiStateToggleButton.setOnValueChangedListener(value -> {
+            onButtonPressed(owDevice, multiStateToggleButton);
+        });
 
     }
 
-    public void updateEnabledState(List<Button> buttons) {
-        for (Button button : buttons) {
-            button.setEnabled(owDevice.isConnected.get());
-        }
+    public void updateEnabledState(MultiStateToggleButton multiStateToggleButton) {
+        multiStateToggleButton.setEnabled(owDevice.isConnected.get());
     }
 
-    public void onButtonPressed(OWDevice mOWDevice, BluetoothUtil bluetoothUtil, int positionIdentifier) {
+    public void onButtonPressed(OWDevice mOWDevice, MultiStateToggleButton multiStateToggleButton) {
+        int positionIdentifier = multiStateToggleButton.getValue();
         int rideModeInt;
         if (mOWDevice.isOneWheelPlus.get()) {
             rideModeInt = positionIdentifier + 4; // ow+ ble value for rideMode 4,5,6,7,8 (delirium)
@@ -126,6 +91,7 @@ public class RideModeView {
                 throw new IllegalStateException("Unknown rideModeInt for ow: " + rideModeInt);
             }
         }
-        mOWDevice.setRideMode(bluetoothUtil,rideModeInt);
+        activity.updateLog("Ridemode changed to:" + rideModeInt);
+        mOWDevice.setRideMode(activity.getBluetoothUtil(),rideModeInt);
     }
 }
